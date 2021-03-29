b7d72fcb59bd9a3c1d7c68db39c606f522f4dcc4
Jennifer Yoon <jyoon94@uwo.ca>
2021-03-29 22:42:26 +0000

    LoginProxy.java edited online with Bitbucket

--- src/main/java/login/LoginProxy.java
+++ src/main/java/login/LoginProxy.java
@@ -2,4 +2,20 @@ package login;
 
 public class LoginProxy {
 
+	private String username, password;
+  	private Login RL;
+  
+  	public LoginProxy(String usr, String pwd) {
+      username = usr;
+      password = pwd;
+      RL = new RealLogin();
+    }
+  
+  	public boolean authenticate() {
+      LoginServer server = LoginServer.getInstance();
+      if (server.verifyCredentials(username, password))
+        RL.authenticate();
+        return true;
+      return false;
+    }
 }
