c22eea4dcb58f44b6c4685b1b76306acaf539162
Jennifer Yoon <jyoon94@uwo.ca>
2021-03-30 05:31:56 +0000

    Selection.java edited online with Bitbucket

--- src/main/java/main/Selection.java
+++ src/main/java/main/Selection.java
@@ -1,5 +1,62 @@
 package main;
 
 public class Selection {
+  
+  String analysisType, country;
+  int startYear, endYear;
+  List<String> viewers;
+  
+  public void setAnalysisType(String selectedType) {
+    analysisType = selectedType;
+  }
+  
+  public void setCountry(String selectedCountry) {
+    if (validateCountry(selectedCountry))
+    	country = selectedCountry;
+  }
+  
+
+  public void setStartYear(int selectedYear) {
+    startYear = selectedYear;
+  }
+  
+
+  public void setEndYear(String selectedYear) {
+    endYear = selectedYear;
+  }
+  
+  public void addViewer(String selectedViewer) {
+    viewers.add(selectedViewer);
+  }
+  
+  public void removeViewer(String selectedViewer) {
+    viewers.remove(selectedViewer);
+  }
+  
+  public String getAnalysisType() {
+    return analysisType;
+  }
+
+  public String getCountry() {
+  return country;
+  }
+
+
+  public int getStartYear() {
+    return startYear;
+  }
+
+  public int getEndYear() {
+    return endYear;
+  }
+
+  private boolean validateCountry(String country) {
+    // open country exclusion file and validate country
+    // return true if valid
+  }
+  
+  private boolean validatePeriod(int startYear, int endYear) {
+  }
+
 
 }
