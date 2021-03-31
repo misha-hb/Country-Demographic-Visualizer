e54f9b1d8c2761a7db88ec0c463ca10f1b824db3
yoonj0 <yoonj0@192.168.2.19>
2021-03-30 21:20:35 -0300

    analysis factory

--- src/main/java/analysis/AnalysisFactory.java
+++ src/main/java/analysis/AnalysisFactory.java
@@ -1,6 +1,31 @@
 package analysis;
 
 public class AnalysisFactory {
-	//testing
+	
+	public Analysis createAnalysis(Selection selection) {
+		Analysis analysisObj;
+		String type = selection.getAnalysisType();
+		
+		//if (type.contentEquals("CO2 Emissions vs Energy Use vs Air Pollution"))
+			//analysisObj = new Analysis1();
+		//else if (type.contentEquals("Air Pollution vs Forest Area"))
+			//analysisObj = new Analysis2();
+		//else if (type.contentEquals("Ratio of CO2 Emissions and GPD per Capita"))
+			//analysisObj = new Analysis3();
+		//else if (type.contentEquals("Average Forest Area"))
+			//analysisObj = new Analysis4();
+		//else if (type.contentEquals("Average Government Expenditure per Capita"))
+			//analysisObj = new Analysis5();
+		//else if (type.contentEquals("Ratio of Hospital Beds and Current Health Expenditure"))
+			//analysisObj = new Analysis6();
+		//else if (type.contentEquals("Current Health Expenditure per Capita vs Mortality Rate"))
+			//analysisObj = new Analysis7();
+		//else
+			//analysisObj = new Analysis8();
+		
+		analysisObj = new Analysis();	// remove later
+		return analysisObj;
+
+	}
 	
 }
