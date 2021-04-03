aaf341e2f41730d3d8d3d3f95d41ec43bb93aa1f
Marshall Mevissen Moore <mmoor69@uwo.ca>
2021-04-03 15:30:54 -0400

    viewer attachment

--- src/main/java/analysis/AnalysisServer.java
+++ src/main/java/analysis/AnalysisServer.java
@@ -2,8 +2,12 @@ package analysis;
 
 import java.util.List;
 
+import main.BarChart;
+import main.LineChart;
 import main.MainUI;
 import main.PieChart;
+import main.Report;
+import main.ScatterChart;
 
 public class AnalysisServer {
 	
@@ -20,9 +24,21 @@ public class AnalysisServer {
 			switch (viewer) {
 			case "Pie Chart" :
 				new PieChart(resultObj);
+				break;
+			case "Bar Chart" :
+				new BarChart(resultObj);
+				break;
+			case "Line Chart" :
+				new LineChart(resultObj);
+				break;
+			case "Scatter Chart" :
+				new ScatterChart(resultObj);
+				break;
+			case "Report" :
+				new Report(resultObj);
+				break;
 			}
 		}
-		
 	
 		if (resultObj != null)
 			resultObj.notifyViewers();
