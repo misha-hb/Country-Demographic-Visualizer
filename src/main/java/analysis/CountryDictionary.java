95b2384ce328b58abdd08c7d28f5c0248fa8738f
Misha <mbutt63@uwo.ca>
2021-04-03 20:24:11 -0400

    Country Dictionary class

---
+++ src/main/java/analysis/CountryDictionary.java
@@ -0,0 +1,36 @@
+package analysis;
+import java.io.IOException;
+import java.util.Hashtable;
+import java.util.List;
+
+public class CountryDictionary {
+	
+	private Hashtable<String, String[]> countriesDict = new Hashtable<String,String[]>();
+	
+	/**
+	   * @param file to be read
+	   * created dictionary from file in the form of (country name: [abbreviation, start year, end year])
+	   * @throws IOException
+	   */
+	  public CountryDictionary() throws IOException {
+		  Reader reader = new Reader();
+		  List<String[]> countriesDatabase = reader.readFile("CountriesFile.txt");
+		  for (int i = 0; i < countriesDatabase.size(); i++) {
+			  String [] valueList = new String[3];
+			  valueList[0] = countriesDatabase.get(i)[5].toLowerCase();
+			  String startYear = countriesDatabase.get(i)[countriesDatabase.get(i).length - 2].toLowerCase();
+			  String endYear = countriesDatabase.get(i)[countriesDatabase.get(i).length - 1].toLowerCase();
+			  if (startYear.compareTo("Now") == 0 || startYear.compareTo("now") == 0)
+				  startYear = "2021";
+			  if (endYear.compareTo("Now") == 0 || endYear.compareTo("now") == 0)
+				  endYear = "2021";
+			  valueList[1] = startYear;
+			  valueList[2] = endYear;
+			  countriesDict.put(countriesDatabase.get(i)[1].toLowerCase(), valueList);
+		  }	 
+	  }
+	  
+	  public Hashtable<String, String[]> getDict() {
+		  return countriesDict;
+	  }
+}
