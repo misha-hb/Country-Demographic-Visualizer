df8f1ecdea0138bfb887a446462c8ce72bdb5d01
Misha <mbutt63@uwo.ca>
2021-04-04 10:55:25 -0400

    line chart

--- src/main/java/main/LineChart.java
+++ src/main/java/main/LineChart.java
@@ -1,7 +1,31 @@
 package main;
 
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.swing.BorderFactory;
+import javax.swing.JPanel;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.block.BlockBorder;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.chart.title.TextTitle;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+
 import analysis.Result;
 import analysis.Subject;
+import analysis.Data;
+import main.MainUI;
 
 public class LineChart implements Viewer {
 	private Result result;
@@ -18,11 +42,72 @@ public class LineChart implements Viewer {
 	}
 
 	public void drawViewer() {
-		String type = result.getType();
-		int[] values = result.getValues();
-		int[] years = result.getYears();
+		XYSeriesCollection dataset = new XYSeriesCollection();
+		
+		List<Data> dataList = result.getData(); 
+		int numOfSeries = dataList.size(); 
+		
+		Iterator<Data> iterator = dataList.iterator();
+		Data data = iterator.next();
+		
+		XYSeries series1 = new XYSeries(data.getType());
+		for (int i = (data.getYears().size()) - 1; i >= 0; i--) {
+			series1.add(data.getYears().get(i), data.getValues().get(i));
+		}
+		dataset.addSeries(series1);
+
+		
+		if (numOfSeries >= 2) {
+			Data next = iterator.next();
+			XYSeries series2 = new XYSeries(next.getType());
+			for (int i = (next.getYears().size()) - 1; i >= 0; i--) {
+				series2.add(next.getYears().get(i), next.getValues().get(i));
+			}
+			dataset.addSeries(series2);
+		}
+		
+		if (numOfSeries == 3) {
+			Data third = iterator.next();
+			XYSeries series3 = new XYSeries(third.getType());
+			for (int i = (third.getYears().size()) - 1; i >= 0; i--) {
+				series3.add(third.getYears().get(i), third.getValues().get(i));
+			}
+			dataset.addSeries(series3);
+		}
 		
 
-		// Draw operation for Line Chart
+		JFreeChart chart = ChartFactory.createXYLineChart(result.getName(), "Year", "", dataset,
+				PlotOrientation.VERTICAL, true, true, false);
+
+		XYPlot plot = chart.getXYPlot();
+
+		XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
+		renderer.setSeriesPaint(0, Color.RED);
+		renderer.setSeriesStroke(0, new BasicStroke(2.0f));
+
+		plot.setRenderer(renderer);
+		plot.setBackgroundPaint(Color.white);
+
+		plot.setRangeGridlinesVisible(true);
+		plot.setRangeGridlinePaint(Color.BLACK);
+
+		plot.setDomainGridlinesVisible(true);
+		plot.setDomainGridlinePaint(Color.BLACK);
+
+		chart.getLegend().setFrame(BlockBorder.NONE);
+
+		chart.setTitle(
+				new TextTitle(result.getName(), new Font("Serif", java.awt.Font.BOLD, 18)));
+
+		ChartPanel chartPanel = new ChartPanel(chart);
+		chartPanel.setPreferredSize(new Dimension(400, 300));
+		chartPanel.setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15));
+		chartPanel.setBackground(Color.white);
+		JPanel panel = MainUI.getPanel();
+		panel.add(chartPanel);
+		
+		
+		
 	}
 }
+
