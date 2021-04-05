00b0c78ab65f42b66eb01c1d09fd2b2a93ceae63
emmyg <emmyg@LAPTOP-SH4U2SEV>
2021-04-05 12:48:32 -0400

    edit

--- src/main/java/analysis/HospitalAndHealthExpenditure.java
+++ src/main/java/analysis/HospitalAndHealthExpenditure.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-// Hospital beds and Health expenditure.
+// Hospital beds and Health expenditure 
 public class HospitalAndHealthExpenditure extends Analysis {
 	
 	public HospitalAndHealthExpenditure() {
