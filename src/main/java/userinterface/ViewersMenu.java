5cc1b2f2e46fd146e29b6e94cda243db03be30a8
emmyg <emmyg@LAPTOP-SH4U2SEV>
2021-04-05 23:18:55 -0400

    comments

--- src/main/java/userinterface/ViewersMenu.java
+++ src/main/java/userinterface/ViewersMenu.java
@@ -7,13 +7,19 @@ import javax.swing.JLabel;
 
 import analysis.Selection;
 
-
+/**
+ * Class will populate the menu that hold all the viewer names
+ */
 public class ViewersMenu extends DropDownMenu {
-		
+	/**
+	 * Constructor for class
+	 * @param labelString
+	 * @param selection
+	 */
 	public ViewersMenu(String labelString, Selection selection) {
 		super(labelString, selection);
 
-		Vector<String> viewers = new Vector<String>();
+		Vector<String> viewers = new Vector<String>(); //Will add each analysis name into the menu
 		viewers.add("Select viewer");
 		viewers.add("Pie Chart");
 		viewers.add("Line Chart");
@@ -22,7 +28,7 @@ public class ViewersMenu extends DropDownMenu {
 		viewers.add("Time Series Chart");
 		viewers.add("Report");
 
-		list = new JComboBox<String>(viewers);
+		list = new JComboBox<String>(viewers);//inserts list of viewers into box for menu
 
 	}
 
