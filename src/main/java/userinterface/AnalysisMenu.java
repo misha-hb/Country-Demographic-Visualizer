e5091eb45fb6473364b9432434c346836600cf00
emmyg <emmyg@LAPTOP-SH4U2SEV>
2021-04-05 23:02:36 -0400

    comments

--- src/main/java/userinterface/AnalysisMenu.java
+++ src/main/java/userinterface/AnalysisMenu.java
@@ -5,14 +5,23 @@ import java.util.Vector;
 import javax.swing.JComboBox;
 
 import analysis.Selection;
-
+/**
+ * Analysis Menu
+ * Adds all the analysis names into the analysis drop down menu
+ *
+ */
 public class AnalysisMenu extends DropDownMenu {
- 
+	
+	/**
+	 * Constructor for class
+	 * @param labelString
+	 * @param selection
+	 */
 	public AnalysisMenu(String labelString, Selection selection) {
 
 		super(labelString, selection);
 		
-		Vector<String> methodsNames = new Vector<String>();
+		Vector<String> methodsNames = new Vector<String>(); //Adds each analysis name into the analysis vector
 		methodsNames.add("Select analysis");
 		methodsNames.add("Average Forest Area");
 		methodsNames.add("Average Government Expenditure on Education");
@@ -23,9 +32,9 @@ public class AnalysisMenu extends DropDownMenu {
 		methodsNames.add("Ratio of Government Expenditure on Education vs Current Health Expenditure");
 		methodsNames.add("Hospital Beds and Current Health Expenditure");
 
-		list = new JComboBox<String>(methodsNames);
+		list = new JComboBox<String>(methodsNames); //creating a list for the selection
 		
-		list.addActionListener(new SelectAction("analysis", selection, list));
+		list.addActionListener(new SelectAction("analysis", selection, list)); //Adds user selection to list
 		
 	}
 
