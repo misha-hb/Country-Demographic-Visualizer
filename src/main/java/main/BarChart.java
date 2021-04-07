ddc4919ec228d320fe1a191b1279da703f43c3da
emmyg <emmyg@LAPTOP-SH4U2SEV>
2021-04-06 22:42:31 -0400

    comments

--- src/main/java/main/BarChart.java
+++ src/main/java/main/BarChart.java
@@ -20,21 +20,37 @@ import org.jfree.chart.plot.CategoryPlot;
 import org.jfree.chart.renderer.category.BarRenderer;
 import org.jfree.data.category.DefaultCategoryDataset;
 
+/**
+ * Represents the bar chart that will appear on the main UI
+ */
+
 public class BarChart implements Viewer {
 	
 	private Result result;
-
+	
+	/**
+	 * Constructor for class 
+	 * will set the result object from the subject object
+	 * @param subject
+	 */
 	public BarChart(Result subject) {
 		this.result = subject;
 		subject.attach(this);
 	}
 
+	/**
+	 * Will update the viewer if viewer class if notified by subject
+	 * @param subject
+	 */
 	public void update(Subject subject) {
 		if (subject.equals(result)) {
 			drawViewer();
 		}
 	}
 
+	/**
+	 * will populate the viewer data and draw the bars for the chart
+	 */
 	public void drawViewer() {
 		
 		List<Data> dataList = result.getData(); //List of data Series required
@@ -68,20 +84,21 @@ public class BarChart implements Viewer {
 		Iterator<Data> DataIterator = dataList.iterator();
 		Data firstData = DataIterator.next();
 	
-		for (int i = (firstData.getYears().size()) - 1; i >= 0; i--) {
+		for (int i = (firstData.getYears().size()) - 1; i >= 0; i--) { //setting the values for each year
 			datasetOne.setValue((firstData.getValues()).get(i), firstData.getType(), Integer.toString((firstData.getYears()).get(i)));
 		}
 		
+		//creating a new bar renderer of first( and/or second) data set and setting the renderer in the plot
 		BarRenderer barrenderer1 = new BarRenderer(); 
-		plot.setDataset(0, datasetOne); 
+		plot.setDataset(0, datasetOne);
 		plot.setRenderer(0, barrenderer1); 
 		
+		//setting the domain and the range axis for the chart
 		CategoryAxis domainAxis = new CategoryAxis("Year"); 
-		
 		plot.setDomainAxis(domainAxis);
 		plot.setRangeAxis(new NumberAxis(""));
 		plot.setRangeAxis(1, new NumberAxis(label));
-		plot.mapDatasetToRangeAxis(0, 0);// 1st data set to 1st y-axis
+		plot.mapDatasetToRangeAxis(0, 0);// 1st data (and/or second) set to 1st y-axis
 		
 		
 		
@@ -91,31 +108,33 @@ public class BarChart implements Viewer {
 		 */
 		if (series > 1) {
 			Data secondData = DataIterator.next();
-			for (int i = (secondData.getYears().size()) - 1; i >= 0; i--) {
+			for (int i = (secondData.getYears().size()) - 1; i >= 0; i--) { //setting the values for each year
 				datasetOne.setValue((secondData.getValues()).get(i), secondData.getType(), Integer.toString((secondData.getYears()).get(i)));
 			}
-			 plot.setRenderer(1, barrenderer1);
+			//setting the renderer in the plot
+			 plot.setRenderer(1, barrenderer1); 
 		}
 		
 		/*
 		 * Creating a bar chart for data with 3 Series
-		 * Third series is on a secondary plane behind
+		 * Third series visually is on a secondary plane behind the first and second
 		 */
 		if (series == 3) {
 			Data thirdData = DataIterator.next();
 			DefaultCategoryDataset datasetTwo = new DefaultCategoryDataset();
-			for (int i = (thirdData.getYears().size()) - 1; i >= 0; i--) {
+			for (int i = (thirdData.getYears().size()) - 1; i >= 0; i--) { //setting the values for each year
 				datasetTwo.setValue((thirdData.getValues()).get(i), thirdData.getType(), Integer.toString((thirdData.getYears()).get(i)));
 			}
-			 BarRenderer barrenderer2 = new BarRenderer(); //A new bar renderer for thrid series
+			 BarRenderer barrenderer2 = new BarRenderer(); //A new bar renderer for third series
 			 plot.setDataset(1, datasetTwo);
-			 plot.setRenderer(1, barrenderer2);
+			 plot.setRenderer(1, barrenderer2); //setting the renderer in the plot
 			 plot.mapDatasetToRangeAxis(0, 0); // 3rd data set to 2nd y-axis
 		}
 		
-		JFreeChart barChart = new JFreeChart(result.getName(), new Font("Serif", java.awt.Font.BOLD, 18), plot, true);
-		ChartPanel chartPanel = new ChartPanel(barChart);
-		chartPanel.setPreferredSize(new Dimension(400, 300));
+		//setting the chart dimension and visual characteristics such as plot
+		JFreeChart barChart = new JFreeChart(result.getName(), new Font("Serif", java.awt.Font.BOLD, 18), plot, true); //setting font
+		ChartPanel chartPanel = new ChartPanel(barChart); // creating a new chart Panel
+		chartPanel.setPreferredSize(new Dimension(400, 300)); 
 		chartPanel.setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15));
 		chartPanel.setBackground(Color.white); 
 		JPanel panel = MainUI.getPanel();
